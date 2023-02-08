fcd9004b29e70ace84a0223a6255c7280d7caf17
ANJANEYULU11 <124799199+ANJANEYULU11@users.noreply.github.com>
2023-02-08 10:13:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled  Free-Range Egg')
 streamlit.text('🥑🍞avacado Toast')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
